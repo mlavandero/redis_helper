d95f904959ec3c710f05ff3a88cd16158d795882
Mario Lavandero <mario@mlavandero.org>
2013-10-02 23:45:40 -0300

    Adds redis_key method to ActiveRecord::Base

--- lib/redis_helper.rb
+++ lib/redis_helper.rb
@@ -1,5 +1,11 @@
 require "redis_helper/version"
 
 module RedisHelper
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  def redis_key(str)
+    "#{self.class.to_s}:#{self.id}:#{str}"
+  end
 end
+
+ActiveRecord::Base.send(:include, RedisHelper)
